a078f03e61fc466e462b2fccf158aaef199857e7
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-03 20:26:51 -0700

    Flip the init

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
     printf("Starting engine\n");
-    if (Engine::getInstance()->init(argc, argv)) {
+    if (!Engine::getInstance()->init(argc, argv)) {
         printf("Failed to init engine\n");
         return -1;
     }
