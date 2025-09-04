49d4b8a76c4cf8b5110d9ffc91ab15606d109273
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-03 22:48:32 -0700

    Added shader function

---
+++ src/shader/shader.cpp
@@ -0,0 +1,95 @@
+#include "shader.hpp"
+
+bool Shader::initShader(const char* vertexFile, const char* fragmentFile) {
+    std::fstream vertFile;
+    std::fstream fragFile;
+    std::string vertexCode;
+    std::string fragmentCode;
+
+    vertFile.exceptions(std::fstream::badbit | std::fstream::failbit);
+    fragFile.exceptions(std::fstream::badbit | std::fstream::failbit);
+
+    try {
+        vertFile.open(vertexFile);
+        fragFile.open(fragmentFile);
+
+        std::stringstream vertStream, fragStream;
+        vertStream << vertFile.rdbuf();
+        fragStream << fragFile.rdbuf();
+
+        vertexCode = vertStream.str();
+        fragmentCode = fragStream.str();
+
+        vertFile.close();
+        fragFile.close();
+    }
+    catch (std::fstream::failure) {
+        fprintf(stderr, "SHADER::ERROR::FILE_LOADING\n");
+        return false;
+    }
+
+    const char* vCode = vertexCode.c_str();
+    const char* fCode = fragmentCode.c_str();
+
+    unsigned int vertex, fragment;
+    int status;
+    char log[512];
+
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vCode, NULL);
+    glCompileShader(vertex);
+    glGetShaderiv(vertex, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        glGetShaderInfoLog(vertex, 512, NULL, log);
+        fprintf(stderr, "SHADER::ERROR::VERTEX::COMPILE::%s\n", log);
+        return false;
+    }
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fCode, NULL);
+    glCompileShader(fragment);
+    glGetShaderiv(fragment, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        glGetShaderInfoLog(fragment, 512, NULL, log);
+        fprintf(stderr, "SHADER::ERROR::FRAGMENT::COMPILE::%s\n", log);
+        return false;
+    }
+
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    glGetProgramiv(ID, GL_LINK_STATUS, &status);
+    if (!status) {
+        glGetProgramInfoLog(ID, 512, NULL, log);
+        fprintf(stderr, "SHADER::ERROR::PROGRAM::LINKING::%s\n", log);
+        return false;
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return true;
+}
+
+void Shader::setInt(const char* name, int value) const {
+    glUniform1i(glGetUniformLocation(ID, name), value);
+}
+
+void Shader::setBool(const char* name, bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name), (int)value);
+}
+
+void Shader::setFloat(const char* name, float value) const {
+    glUniform1f(glGetUniformLocation(ID, name), value);
+}
+
+void Shader::setVec3(const char* name, glm::vec3 value) const {
+    glUniform3fv(glGetUniformLocation(ID, name), 1, &value[0]);
+}
+
+void Shader::setMat3(const char* name, glm::mat3 value) const {
+    glUniformMatrix3fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &value[0][0]);
+}
+
+void Shader::useProgram() { glUseProgram(ID); }
---
+++ src/shader/shader.hpp
@@ -0,0 +1,34 @@
+#ifndef _SHADER_H_
+#define _SHADER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+// #include <glm/gtc/type_ptr.hpp>
+
+class Shader {
+    public:
+        Shader() {}
+
+        bool initShader(const char* vertexFile, const char* fragmentFIle);
+        
+        void useProgram();
+
+        void setInt(const char* name, int value) const;
+        void setBool(const char* name, bool value ) const;
+        void setFloat(const char* name, float value) const;
+        void setVec3(const char* name, glm::vec3 value) const;
+        void setMat3(const char* name, glm::mat3 value) const;
+        
+    private:
+        unsigned int ID;
+};
+
+#endif // _SHADER_H_
