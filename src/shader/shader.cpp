faad57b732e5ab79ae86c8150dae858ef134f2bc
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-04 13:14:11 -0700

    Fix setMat3 to setMat4 for correct model transformation

--- src/shader/shader.cpp
+++ src/shader/shader.cpp
@@ -88,8 +88,8 @@ void Shader::setVec3(const char* name, glm::vec3 value) const {
     glUniform3fv(glGetUniformLocation(ID, name), 1, &value[0]);
 }
 
-void Shader::setMat3(const char* name, glm::mat3 value) const {
-    glUniformMatrix3fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &value[0][0]);
+void Shader::setMat4(const char* name, glm::mat4 value) const {
+    glUniformMatrix4fv(glGetUniformLocation(ID, name), 1, GL_FALSE, &value[0][0]);
 }
 
 void Shader::use() { glUseProgram(ID); }
