eb4044cf0e9f1c4e23c1badee999cb7c6d1bcc2c
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-04 13:46:39 -0700

    Comment out logging

--- src/timer/timer.cpp
+++ src/timer/timer.cpp
@@ -4,7 +4,7 @@ float Timer::getElapsed() {
     auto now = std::chrono::high_resolution_clock::now();
     float elapsed = std::chrono::duration_cast<std::chrono::microseconds>(now - last_time).count() / 1000.f;
     last_time = now;
-    printf("Elapsed: %f\n", elapsed);
+    // printf("Elapsed: %f\n", elapsed);
     return elapsed;
 }
 
