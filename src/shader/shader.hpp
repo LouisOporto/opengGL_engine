3c69ab5619403ca121c49929f1735c744f8ec7bc
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-04 23:13:23 -0700

    Switch out the const char* name for std::string, setup a easier way to setup light types

--- src/shader/shader.hpp
+++ src/shader/shader.hpp
@@ -21,12 +21,15 @@ class Shader {
         
         void use();
 
-        void setInt(const char* name, int value) const;
-        void setBool(const char* name, bool value ) const;
-        void setFloat(const char* name, float value) const;
-        void setVec3(const char* name, glm::vec3 value) const;
-        void setMat4(const char* name, glm::mat4 value) const;
-        
+        void setInt(const std::string name, int value) const;
+        void setBool(const std::string name, bool value ) const;
+        void setFloat(const std::string name, float value) const;
+        void setVec3(const std::string name, glm::vec3 value) const;
+        void setMat4(const std::string name, glm::mat4 value) const;
+       
+        void setSpotLight(const std::string name, glm::vec3 pos, glm::vec3 direction, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float cutOff, float outerCutOff, float constant, float linear, float quadratic);
+        void setPointLight(const std::string name, glm::vec3 pos, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic);
+        void setDirLight(const std::string name, glm::vec3 direction, glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
     private:
         unsigned int ID;
 };
