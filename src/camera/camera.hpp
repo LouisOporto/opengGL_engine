f707ddd6a7bc312458a095301cf6cc0ff8b683cb
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-04 23:32:56 -0700

    Return front camera

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -36,6 +36,7 @@ class Camera {
         glm::mat4 getLookAt() { return glm::lookAt(m_pos, m_pos + m_front, m_up); }
         glm::mat4 getPerspective() { return glm::perspective(glm::radians(m_fov), 800 / 600.f, 0.1f, 100.0f); }
         glm::vec3 getPos() { return m_pos; }
+        glm::vec3 getFront() { return m_front; }
     private:
         void updateCameraVectors();
 
