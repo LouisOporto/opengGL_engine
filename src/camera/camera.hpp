a21e70442200919d38488a958fbdc19a625c7706
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-05 19:32:18 -0700

    Relative updating of screen width and height for correct ratio change

--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -22,10 +22,12 @@ enum DIRECTION {
 
 class Camera {
     public:
-        Camera(glm::vec3 position = {0.0f, 0.0f, 3.0f}, glm::vec3 front = {0.0f, 0.0f, -1.0f}, glm::vec3 up = {0.0, 1.0, 0.0}): m_fov(FOV), m_pitch(PITCH), m_yaw(YAW) {
+        Camera(int scr_w, int scr_h, glm::vec3 position = {0.0f, 0.0f, 3.0f}, glm::vec3 front = {0.0f, 0.0f, -1.0f}, glm::vec3 up = {0.0, 1.0, 0.0}): m_fov(FOV), m_pitch(PITCH), m_yaw(YAW) {
             m_front = front;
             m_pos = position;
             m_worldUp = up;
+            SCR_W = scr_w;
+            SCR_H = scr_h;
             updateCameraVectors();
         }
 
@@ -37,6 +39,9 @@ class Camera {
         glm::mat4 getPerspective() { return glm::perspective(glm::radians(m_fov), SCR_W / (float)SCR_H, 0.1f, 100.0f); }
         glm::vec3 getPos() { return m_pos; }
         glm::vec3 getFront() { return m_front; }
+
+        void setWidth(int value) { SCR_W = value; }
+        void setHeight(int value) { SCR_H = value; }
     private:
         void updateCameraVectors();
 
