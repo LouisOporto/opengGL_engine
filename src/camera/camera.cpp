834660c93ab7b6815ad7ecf82994ac493baf010f
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-09-10 19:14:13 -0700

    Implement simple roll, but issue with using left and right directions

--- src/camera/camera.cpp
+++ src/camera/camera.cpp
@@ -2,6 +2,7 @@
 
 void Camera::handleKeyInput(DIRECTION dir, float deltaTime) {
     float speed = deltaTime * VELOCITY;
+    float roll = deltaTime * ROLLING;
     switch (dir) {
         case FORWARD: m_pos += m_front * speed; break;
         case BACKWARD: m_pos -= m_front * speed; break;
@@ -9,7 +10,11 @@ void Camera::handleKeyInput(DIRECTION dir, float deltaTime) {
         case RIGHT: m_pos += m_right * speed; break;
         case UP: m_pos += m_up * speed; break;
         case DOWN: m_pos -= m_up * speed; break;
+
+        case ROLL_LEFT: m_roll -= roll; break;
+        case ROLL_RIGHT: m_roll += roll; break;
     }
+    updateCameraVectors();
 }
 
 void Camera::handleMouseInput(float xoffset, float yoffset, bool constrainPitch) {
@@ -35,10 +40,16 @@ void Camera::handleScrollInput(float yoffset) {
 
 void Camera::updateCameraVectors() {
     glm::vec3 front;
+    printf("Yaw: %f, Pitch: %f, Roll: %f\n", m_yaw, m_pitch, m_roll);
     front.x = cos(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
     front.y = sin(glm::radians(m_pitch));
     front.z = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
+
+    glm::mat4 roll_mat = glm::rotate(glm::mat4(1.0f), glm::radians(m_roll), front);
+
     m_front = glm::normalize(front);
     m_right = glm::normalize(glm::cross(m_front, m_worldUp));
     m_up = glm::normalize(glm::cross(m_right, m_front));
+
+    m_up = glm::mat3(roll_mat) * m_up;
 }
