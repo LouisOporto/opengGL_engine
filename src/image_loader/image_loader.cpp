7d89b43caa26f7c416e691d5e31ec3a7f32d4044
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-09-15 20:31:24 -0700

    Change usage of image_loader

--- src/image_loader/image_loader.cpp
+++ src/image_loader/image_loader.cpp
@@ -1,22 +1,31 @@
 #include "image_loader.hpp"
 
 ImageLoader* ImageLoader::m_instance = nullptr;
-unsigned int ImageLoader::loadImage(const char* filepath, int channels) {
+
+unsigned int ImageLoader::loadImage(const char *path, const std::string &directory) {
+    std::string filepath = std::string(path);
+    filepath = directory + '/' + filepath;
+
     unsigned int texture;
     glGenTextures(1, &texture);
+
     int width, height, nrChannels;
-    unsigned char* data = stbi_load(filepath, &width, &height, &nrChannels, channels);
+    unsigned char* data = stbi_load(filepath.c_str(), &width, &height, &nrChannels, 0);
     if (data) {
         glBindTexture(GL_TEXTURE_2D, texture);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, (channels == 4 ? GL_RGBA : GL_RGB), GL_UNSIGNED_BYTE, data);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, (nrChannels == 4 ? GL_RGBA : GL_RGB), GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
         
         glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
         glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
         glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTextureParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        stbi_image_free(data);
     }
-    stbi_image_free(data);
+    else {
+        printf("Textures failed to laod path: %s", filepath);
+        stbi_image_free(data);
+    }
 
     return texture;
 }
--- src/image_loader/image_loader.hpp
+++ src/image_loader/image_loader.hpp
@@ -2,12 +2,13 @@
 #define _IMAGE_LOADER_H_
 #include "stb_image.h"
 #include <GL/glew.h>
+#include <string>
 
 class ImageLoader {
     public:
         static ImageLoader* getInstance() { return m_instance = m_instance != nullptr ? m_instance : new ImageLoader(); }
         
-        unsigned int loadImage(const char* filepath, int channels);
+        unsigned int loadImage(const char* filepath, const std::string &directory);
     private:
         ImageLoader() {}
         static ImageLoader* m_instance;
