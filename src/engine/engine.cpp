7a23442f0b8b6b333fcc40d4003686369284ec98
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-11-02 00:14:15 -0700

    Remove unused code

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -45,16 +45,6 @@ bool Engine::init(int argc, char* argv[]) {
     glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES), VERTICES, GL_STATIC_DRAW);
     
-    // glBindVertexArray(m_objectVAO);
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
-    // glEnableVertexAttribArray(0);
-    
-    // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 3));
-    // glEnableVertexAttribArray(1);
-    
-    // glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)(sizeof(float) * 6));
-    // glEnableVertexAttribArray(2);
-    
     glBindVertexArray(m_lightVAO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 8, (void*)0);
     glEnableVertexAttribArray(0);
