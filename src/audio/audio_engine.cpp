7125d0e59a2bd0b6a1241c552d60ba75ca7c520c
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-11-10 17:49:09 -0800

    New song struct, parameter convetion changed to align with better use case

--- src/audio/audio_engine.cpp
+++ src/audio/audio_engine.cpp
@@ -20,33 +20,46 @@ bool AudioEngine::init() {
     return true;
 }
 
-void AudioEngine::loadBank(std::string filename, const std::string& directory) {
-    if (m_banks.count(filename) == 0) {
+void AudioEngine::loadBank(std::string bankName, const std::string& directory) {
+    // The filename is used to locate the bank location, but the actual string that maps is just the concatenated to the file name
+    // Ex. Filename: "Master.bank" String Mapping: "Master"
+    std::string filename = bankName + ".bank";
+    if (m_banks.count(bankName) == 0) {
         std::string path = directory + "/" + filename;
-        if (FMOD_Studio_System_LoadBankFile(m_system, path.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &m_banks[filename]) != FMOD_OK) {
+        if (FMOD_Studio_System_LoadBankFile(m_system, path.c_str(), FMOD_STUDIO_LOAD_BANK_NORMAL, &m_banks[bankName]) != FMOD_OK) {
             Logger::Error("Error loading bank from directory: %s", path.c_str());
         }
         Logger::Log("Loading bank file: %s", filename.c_str());
     }
 }
 
-void AudioEngine::dropBank(std::string filename) {
-    if (m_banks.count(filename) != 0) {
-        FMOD_Studio_Bank_Unload(m_banks[filename]);
-        m_banks.erase(filename);
-        Logger::Log("Drop file: %s", filename.c_str());
+void AudioEngine::dropBank(std::string bankName) {
+    if (m_banks.count(bankName) != 0) {
+        if (FMOD_Studio_Bank_Unload(m_banks[bankName]) != FMOD_OK) {
+            Logger::Error("Failed to unload bank: %s", bankName);
+        }
+        m_banks.erase(bankName);
+        Logger::Log("Drop file: %s", bankName.c_str());
     }
 }
 
-void AudioEngine::playByIndex(std::string filename, int numberFiles, unsigned int index = 0) {
-    FMOD_STUDIO_EVENTDESCRIPTION** descript = new FMOD_STUDIO_EVENTDESCRIPTION*[numberFiles];
+void AudioEngine::playByIndex(std::string bankName, unsigned int index) {
+    // If we want index i. Get event list of i + 1
+    FMOD_STUDIO_EVENTDESCRIPTION** descript = new FMOD_STUDIO_EVENTDESCRIPTION*[index + 1];
     int numberLoaded = 0;
 
-    FMOD_Studio_Bank_GetEventList(m_banks[filename], descript, numberFiles, &numberLoaded);
+    FMOD_Studio_Bank_GetEventList(m_banks[bankName], descript, index + 1, &numberLoaded);
     Logger::Warn("Number of events in given bank file: %d", numberLoaded);
 
     if (FMOD_Studio_EventDescription_CreateInstance(descript[index], &instance) != FMOD_OK) {
         Logger::Error("Failed to create instance");
+    }
+
+    int length = 0;
+    if (FMOD_Studio_EventDescription_GetLength(descript[index], &length) == FMOD_OK) {
+        int minutes = length / 60000;
+        int seconds = length / 1000 % 60;
+        Logger::Warn("Total length of event: %d:%d", minutes, seconds);
     }
 
     if (FMOD_Studio_EventInstance_Start(instance) != FMOD_OK) {
@@ -70,8 +83,9 @@ void AudioEngine::playByPath(std::string index) {
         Logger::Error("Failed to create instance");
     }
 
-    if (FMOD_Studio_EventInstance_SetParameterByName(instance, "Parameter 1", 1.0f, false) != FMOD_OK) {
-        Logger::Error("Fail to set parameter");
+    int length = 0;
+    if (FMOD_Studio_EventDescription_GetLength(descript, &length) != FMOD_OK) {
+        Logger::Warn("Total length of event: %d", length);
     }
 
     if (FMOD_Studio_EventInstance_Start(instance) != FMOD_OK) {
--- src/audio/audio_engine.hpp
+++ src/audio/audio_engine.hpp
@@ -2,23 +2,48 @@
 #define _AUDIO_ENGINE_H_
 
 #include <map>
+#include <vector>
 #include <string>
 
 #include "../../FMOD/api/studio/inc/fmod_studio.h"
 #include "../../FMOD/api/core/inc/fmod.h"
 #include "../engine/logger.hpp"
 
+
+
+struct Music {
+    Music(std::string songName, int totalMins, int totalSecs, int currentMins = 0, int currentSecs = 0, bool paused = false, bool stop = false, bool released = false) {
+        name = songName;
+        totalMin = totalMins;
+        totalSec = totalSecs;
+        curMin = currentMins;
+        curSec = currentSecs;
+        isPaused = paused;
+        isStop = stop;
+        isReleased = released;
+    }
+
+    std::string name;
+    int totalMin;
+    int totalSec;
+    int curMin;
+    int curSec;
+    bool isPaused;
+    bool isStop;
+    bool isReleased;
+};
+
 class AudioEngine {
     public:
         static AudioEngine* getInstance() { return m_instance = m_instance != nullptr ? m_instance : new AudioEngine(); }
         bool init();
 
-        void loadBank(std::string filename, const std::string& directory);
-        void dropBank(std::string filename);
+        void loadBank(std::string bankName, const std::string& directory);
+        void dropBank(std::string bankName);
 
-        void playByIndex(std::string filename, int numberFiles, unsigned int index);
+        void playByIndex(std::string bankName, unsigned int index = 0);
         void playByPath(std::string path);
-        void playTest(std::string filename) { playByIndex(filename, 10, 0); }
+        void playTest(std::string bankName) { playByIndex(bankName); }
 
         void setInstanceParemeter(std::string parameter, float value);
         void setTimelinePosition(int value);
@@ -33,7 +58,9 @@ class AudioEngine {
         static AudioEngine* m_instance;
         FMOD_STUDIO_SYSTEM* m_system = nullptr;
         std::map<std::string, FMOD_STUDIO_BANK*> m_banks;
+        std::map<std::string, FMOD_STUDIO_EVENTINSTANCE*> m_eventInstances;
         FMOD_STUDIO_EVENTINSTANCE* instance;
+        std::map<std::string, std::vector<unsigned int>> BANKMAP; // Relative bank directory
 };
 
 #endif // _AUDIO_ENGINE_H_
