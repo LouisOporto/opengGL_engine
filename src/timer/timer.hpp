41455db7545dff8a4432ac934d7d019424dbcf58
LouisOporto <louisoporto042@csu.fullerton.edu>
2025-11-16 20:30:46 -0800

    Formatted with clang-format

--- src/timer/timer.hpp
+++ src/timer/timer.hpp
@@ -1,16 +1,17 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
-#include <stdio.h> // for logging
+#include <stdio.h>  // for logging
+
 #include <chrono>
 
 class Timer {
-    public:
-        Timer(): last_time(std::chrono::high_resolution_clock::now()) {}
-        float getElapsed();
-        
-    private:
-        std::chrono::_V2::high_resolution_clock::time_point last_time;
+   public:
+    Timer() : last_time(std::chrono::high_resolution_clock::now()) {}
+    float getElapsed();
+
+   private:
+    std::chrono::_V2::high_resolution_clock::time_point last_time;
 };
 
-#endif // _TIMER_H_
+#endif  // _TIMER_H_
